0e45b1364ab07d40d3a1ee4c3c2ca4ded06d22a7
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-05-16 17:56:11 +0100

    added google details

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -5,8 +5,8 @@ import GoogleProvider from 'next-auth/providers/google';
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: '',
-      clientSecret: ''
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
   async session({ session }) {
