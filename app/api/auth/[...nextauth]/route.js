a280d7f6f2253d586dec2f257cf6c1a107b7177b
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-05-16 18:21:53 +0100

    set up connection to mongo

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth/next';
 import Google from 'next-auth/providers/google';
 import GoogleProvider from 'next-auth/providers/google';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
@@ -9,13 +10,26 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
   ],
+
   async session({ session }) {
 
   },
+
   async signIn({ profile }) {
     try {
       //serverless route/ Lambda function. Only opened when called
+      await connectToDB()
 
+      //check if user exists already
+
+
+      // create new user and add to database
+
+      return true
+    }
+    catch (error) {
+      console.log(error);
+      return false;
     }
   }
 })
