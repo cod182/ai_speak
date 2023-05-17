d602db54632495843753b3dcf575c4874ded43a8
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-05-17 16:39:44 +0100

    func to create finished

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,13 +1,19 @@
+import Prompt from '@models/prompt';
 import { connectToDb } from '@utils/database';
-
 
 
 export const POST = async (req, res) => {
   const { userId, prompt, tag } = await req.json();
   try {
     await connectToDb();
-
+    const newPrompt = new Prompt({
+      Creator: userId,
+      prompt,
+      tag
+    })
+    await new Prompt.save();
+    return new Response(JSON.stringify(newPrompt), { status: 201 })
   } catch (error) {
-    console.log(error)
+    return new Response('Failed to create new prompt', { status: 500 })
   }
 }
