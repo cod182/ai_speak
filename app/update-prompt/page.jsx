9c1c9ada690fa558a7bf51b26c0a4ee3218f2648
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-05-18 16:14:12 +0100

    created edit page

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,9 +1,65 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+
 import Form from '@components/Form';
 
-Form;
+const EditPrompt = () => {
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get('id');
+  const router = useRouter();
 
-const updatePrompt = () => {
-  return <div>page</div>;
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: '', tag: '' });
+
+  useEffect(() => {
+    const getPromptDetail = async () => {
+      const res = await fetch(`/api/prompt/${promptId}`);
+
+      const data = await res.json();
+
+      setPost({ prompt: data.prompt, tag: data.tag });
+    };
+    if (promptId) {
+      getPromptDetail();
+    }
+  }, [promptId]);
+
+  // const createPrompt = async (e) => {
+  //   e.preventDefault();
+  //   setIsSubmitting(true);
+
+  //   try {
+  //     const response = await fetch('/api/prompt/new', {
+  //       method: 'POST',
+  //       body: JSON.stringify({
+  //         prompt: post.prompt,
+  //         userId: session?.user.id,
+  //         tag: post.tag,
+  //       }),
+  //     });
+
+  //     if (response.ok) {
+  //       router.push('/');
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setIsSubmitting(false);
+  //   }
+  // };
+
+  return (
+    <Form
+      type="Edit"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={() => {}}
+    />
+  );
 };
 
-export default updatePrompt;
+export default EditPrompt;
