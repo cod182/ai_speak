3e92382fbbbefde1732df715bb0ab0cd4060e368
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-05-18 15:20:11 +0100

    added api endpoint for user posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,5 +1,39 @@
-const Profile = () => {
-  return <div>profile</div>;
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import Profile from '@components/profile';
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+
+  const [posts, setPosts] = useState([]);
+  const handleEdit = () => {};
+
+  const handleDelete = async () => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await res.json();
+      setPosts(data);
+    };
+    if (session?.user.id) {
+      fetchPosts();
+    }
+  }, []);
+
+  return (
+    <Profile
+      name="My"
+      decs="Welcome to your personalised profile page"
+      data={posts}
+      handelEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
