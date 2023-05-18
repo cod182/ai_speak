5c21180e30e3eb7618677ad7599e736a205d2b0e
Codie Stephens-Evans <75341001+cod182@users.noreply.github.com>
2023-05-18 16:22:00 +0100

    added delete funciton for posts

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -15,7 +15,18 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async (post) => {};
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure?');
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, { method: 'DELETE' });
+        const filteredPosts = myPosts.filter((p) => p._id != post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
